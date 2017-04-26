13d0efc81924d95e1bcd620166c96e58973571f1
oligsei <oligsei.reznikov@gmail.com>
2017-04-26 19:10:22 +0300

    Added first test

---
+++ src/test/java/javaschool/app/converters/temperature/FahrenheitConverterTest.java
@@ -0,0 +1,20 @@
+package javaschool.app.converters.temperature;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FahrenheitConverterTest {
+    private final FahrenheitConverter converter = new FahrenheitConverter();
+
+    @Test
+    public void from() throws Exception {
+        assertEquals(converter.from(10), 50.0);
+    }
+
+    @Test
+    public void to() throws Exception {
+        assertEquals(converter.to(50), 10.0);
+    }
+
+}
