46e2b7a4dafbea21b4d52da40eaa06a3d575bdbd
oligsei <oligsei.reznikov@gmail.com>
2017-04-26 18:06:32 +0300

    fixed imperial formatter

--- src/main/java/javaschool/app/formatters/ImperialFormatter.java
+++ src/main/java/javaschool/app/formatters/ImperialFormatter.java
@@ -3,7 +3,7 @@ package javaschool.app.formatters;
 public class ImperialFormatter extends Formatter {
     public String format(double value, String suffix) {
         int whole = (int) value;
-        int denominator = 256;
+        int denominator = 10000;
         int numerator = (int) ((value - whole) * denominator);
 
         if (numerator == 0) {
@@ -14,7 +14,7 @@ public class ImperialFormatter extends Formatter {
             denominator /= 2;
         }
 
-        if (whole != 0) {
+        if (whole == 0) {
             return String.format("%d/%d %s", numerator, denominator, suffix);
         } else {
             return String.format("%d %d/%d %s", whole, numerator, denominator, suffix);
