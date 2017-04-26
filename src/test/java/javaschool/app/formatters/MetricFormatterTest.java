b6f64b6da0f392056171b67675fc16f01c086ef7
oligsei <oligsei.reznikov@gmail.com>
2017-04-26 20:26:50 +0300

    metric tests should be parametrized

--- src/test/java/javaschool/app/formatters/MetricFormatterTest.java
+++ src/test/java/javaschool/app/formatters/MetricFormatterTest.java
@@ -2,13 +2,38 @@ package javaschool.app.formatters;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class MetricFormatterTest {
     private final Formatter formatter = new MetricFormatter();
 
+    @Parameterized.Parameter()
+    public double value;
+
+    @Parameterized.Parameter(1)
+    public String suffix;
+
+    @Parameterized.Parameter(2)
+    public String expected;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {10.5, "s", "10,50 s"},
+                {10.25, "s", "10,25 s"},
+                {10.125, "s", "10,13 s"},
+        });
+    }
+
+
     @Test
     public void format() throws Exception {
-        Assert.assertEquals("10,12 s", formatter.format(10.123, "s"));
+        Assert.assertEquals(expected, formatter.format(value, suffix));
     }
 
 }
